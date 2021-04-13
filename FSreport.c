2343ac041a636fa3c321857feb4699b996c2edad
Justin Stewart <jstewa28@fred.socs.uoguelph.ca>
2021-04-13 14:40:26 -0400

    added empty functions and edited makefile

---
+++ FSreport.c
@@ -0,0 +1,41 @@
+#include "header.h"
+
+int main(int argc, char const *argv[]) {
+	printf("HERE WE GO AGAIN\n");
+	return 0;
+}
+
+int directoryLevelCount(char *path, char *extension, int count) {
+  return 0;
+}
+
+int max(int a, int b) {
+  return 0;
+}
+
+void treeFileInfo(struct dirent dirp, char *path, int level, struct levelTreeFiles *levelsArray, int fileOrDir, char *dirName) {
+}
+
+int treeDirectoryTravel(char *path, char *extension, int level, struct levelTreeFiles *levelsArray) {
+  return 0;
+}
+
+int strcmpFunc(const void *a, const void *b) {
+  return 0;
+}
+
+int inodeDirectoryTravel(char *path, char *extension, int level, struct levelInodeFiles *levelsArray) {
+  return 0;
+}
+
+void inodeFileInfo(struct dirent dirp, char *path, int level, struct levelInodeFiles *levelsArray, char *directoryName) {
+  
+}
+
+int numCmpFunc(const void *a, const void *b) {
+  return 0;
+}
+
+int inodeDirSort(const void *a, const void *b) {
+  return 0;
+}
--- makefile
+++ makefile
@@ -3,3 +3,6 @@ CFLAGS = -I. -std=c99 -Wall -Wpedantic -ansi -lm
 
 FSreport: FSreport.c
 	$(CC) -o FSreport FSreport.c $(CFLAGS)
+
+clean:
+	rm FSreport
